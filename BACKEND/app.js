f57803723e4e4105a98350c3449d20c11d70bbc7
Bhavy Patel <bhavypatel101005@gmail.com>
2026-06-22 15:16:39 +0530

    cors fix

--- BACKEND/app.js
+++ BACKEND/app.js
@@ -20,10 +20,15 @@ const app = express();
 
 connectToDb();
 
-app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}));
+app.use(
+    cors({
+        origin: [
+            "http://localhost:5173",
+            "https://quickride-rho.vercel.app/"
+        ],
+        credentials: true
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
